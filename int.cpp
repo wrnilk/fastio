3e9745a246629748ebe4636b63d8241973f64d42
wrnilk <73866482+wrnilk@users.noreply.github.com>
2024-10-25 17:13:18 +0900

    Create int.cpp

---
+++ int.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <unistd.h>
+
+const int MAX = 1 << 20;
+
+char buf[MAX];
+char *cur = buf;
+
+char readChar() {
+  if(cur == buf + MAX) read(0, cur = buf, MAX);
+  return *cur++;
+}
+
+int readInt() {
+  int tmp = 0;
+  char c = readChar(), flag = 0;
+  while(~c & 16 && c != '-') c = readChar();
+  if(c == '-') flag = 1, c = readChar();
+  for(;c & 16;tmp = 10 * tmp + (c & 15), c = readChar());
+  return flag ? -tmp : tmp;
+}
+
+int main() {
+    read(0, buf, MAX);
+    
+    int n = readInt();
+    long long ans = 0;
+    for(int i = 0;i < n;i++) ans += readInt();
+    printf("%d\n%lld\n", n, ans);
+}
